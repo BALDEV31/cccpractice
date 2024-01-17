c0e20175a8ccfdfef2367d6b636caf65b141503f
Baldev <baldev.tiwari@cybercom.in>
2024-01-17 18:03:31 +0530

    practiced methods of string

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
 // echo strlen($x);
 
 // str_replace is used to replace the string given with the target string
-    // $x = "hello world";
+    // $x = "hello worhellold hellohello";
     // echo str_replace("hello","hi",$x);
     // str_replace($search, $replace, $subject) in this the order should be maintained for the subject
 
@@ -35,7 +35,47 @@
     //by using this we can trim the string from start and end by providing the seperators to trim by default it is space
 
 
-    //123456
+    //implode is used to create a string out of array elements with defined seperator
+    //join() is similar to implode
+    // $array = ["h","e","l","l","o"];
+    // echo implode("+",$array);
+
+    //it is just the opposite of the implode it creates a array from the given string with the given delimiter
+    //in this echo is not used but print_r will be used
+    // $str ="array1,array2,array3";
+    // print_r(explode('+',$str)) ;
+
+    //Converts special characters to HTML entities.
+    //htmlspecialchars and htmlentities both are same.
+    // $input = '<script>alert("Hello, world!");</script>';
+    // $output = htmlspecialchars($input);
+    // echo $output;
+    
+
+    // $input = '<script>alert("Hello, world!");</script>';
+    // $output = htmlentities($input);
+    // echo $output;
+    
+    //str_repeat is used to repeat a string a particular number of times.
+    // $str = "hello, ";
+    // echo str_repeat($str,5);
+
+    //strrev($string):
+    //Reverses a string.
+    // $rev = 11234;
+    // echo strrev($rev);
+
+    //it randomly shuffles the string
+    // $inputString = "Hello, World!";
+    // $shuffledString = str_shuffle($inputString);
+    // echo $shuffledString;
+
+    // str_split($string, $length): Converts a string to an array, breaking it into smaller chunks.
+    // $inputString = "Hello, World!";
+    // $arrayOfCharacters = str_split($inputString,5);
+    // print_r($arrayOfCharacters);
+
+    
     
 
 ?>
