11c5ce3ff435fcee8347d3301dc5107f70868807
Baldev_Tiwari <baldev.tiwari@cybercom.in>
2024-01-19 14:42:49 +0530

    added more exercise and modified some

--- practice_work/conditional.php
+++ practice_work/conditional.php
@@ -1,3 +1,65 @@
 <?php 
+// $age = 17;
+// if($age>18){
+//     echo "you are eligible to vote";
+// }
+
+
+//if else statements
+// if($age>18){
+//     echo "you are eligible to drive";
+// }
+// else
+//     echo "you are not eligible to drive";
+
+
+//elseif statements..
+// $grade = 91;
+// if($grade<30){
+//     echo "you are failed";
+// }elseif($grade <=50){
+//     echo "you received a grade D";
+// }elseif($grade <= 70){
+//     echo "you received a grade C";
+// }elseif($grade <= 80){
+//     echo "you received a grade B";
+// }elseif($grade <= 90){
+//     echo "you received a grade A";
+// }elseif($grade <= 100){
+//     echo "you received a grade A+";
+// }
+
+
+//nested if statements
+// $cloudy = true;
+// $lightning = false;
+// if($cloudy){
+//     if($lightning){
+//         echo "the is a high chance of rain";
+//     }
+//     else echo "the chances are low";
+// }
+// else echo "there are no chances of rain";
+
+
+//switch case statements
+// $month_of_year ="januar";
+// switch($month_of_year){
+//     case "january": echo "the month is january";break;
+//     case "february": echo "the month is february";break;
+//     case "march": echo "the month is march";break;
+//     case "april": echo "the month is april";break;
+//     case "may": echo "the month is may";break;
+//     case "june": echo "the month is june";break;
+//     case "july": echo "the month is july";break;
+//     case "august": echo "the month is august";break;
+//     case "september": echo "the month is september";break;
+//     case "october": echo "the month is october";break;
+//     case "november": echo "the month is november";break;
+//     case "december": echo "the month is december";break;
+//     default :echo "enter a correct month";
+// }
+// The break statement terminates the switch statement once a match is found. If omitted, execution would continue to the next case.
+// The default case is executed if none of the specified cases match.
 
 ?>
---
+++ practice_work/loops.php
@@ -0,0 +1,45 @@
+<?php
+    // Loops in PHP allow you to execute a block of code repeatedly
+    //for loop
+    //this loop will print the table of 2 from 1 to 10.
+    // for($i=1;$i<=10;$i++){
+    //     echo (2*$i)."<br>";
+    // }
+echo "<pre>";
+    // $n = 5;
+    // for($i=1;$i<=$n;$i++){
+    //   echo str_repeat(" ",$n-$i);
+    //   echo str_repeat("*",2*$i-1);
+    //   echo PHP_EOL;
+    // }
+
+
+//while loop
+// The while loop executes a block of code as long as the specified condition is true.
+    //     $count = 10;
+    //     while(true){
+    //         echo $count--."<br>";
+    //         if($count==0){
+    //             return false;  
+    //     }
+    // }
+
+//do while loop
+//this loop will run atleast one time before checking for the condition.
+    // $i = 1;
+    // do {
+    //   echo $i++."<br>";
+    // } while ($i < 6);
+
+
+//for each loop
+// The foreach loop is specifically designed for iterating over arrays.
+        // $days = ["mon","tue","wed","thru","fri","sat"];
+        // foreach($days as $day){
+        //     echo "the day is :".$day."<br>";
+        // }
+
+        
+echo "</pre>";
+
+?>
